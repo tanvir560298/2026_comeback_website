da742260314aad960fd40adadfdda173cc21a6a4
Tanvir Ahmad <tanvirahmad@Tanvirs-MacBook-Air-2.local>
2026-01-01 01:43:43 -0800

    update homepage text

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "PASTE HERE",
-  authDomain: "PASTE HERE",
-  projectId: "PASTE HERE",
-  storageBucket: "PASTE HERE",
-  messagingSenderId: "PASTE HERE",
-  appId: "PASTE HERE",
+  apiKey: "AIzaSyDba5Vtm2gyT7DCW4N2UQpIorayU7z9LD0",
+  authDomain: "ta-learning-platform.firebaseapp.com",
+  projectId: "ta-learning-platform",
+  storageBucket: "ta-learning-platform.firebasestorage.app",
+  messagingSenderId: "433031981690",
+  appId: "1:433031981690:web:40704f18a0ddfcdc8fe9c3",
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
+// Analytics (safe)
+if (typeof window !== "undefined") {
+  isSupported().then((yes) => {
+    if (yes) getAnalytics(app);
+  });
+}
